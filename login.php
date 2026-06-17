d66d46f38a71399c39c425b79f460ab6ba896bd4
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-17 17:28:01 +0330

    74-login

--- login.php
+++ login.php
@@ -1,5 +1,45 @@
+<?php
+
+session_start();
+include "database/pdo_connection.php";
+
+if (isset($_SESSION['user'])) {
+    unset($_SESSION['user']);
+}
+
+$error = "";
+
+if (
+    isset($_POST['email']) && $_POST['email'] !== "" &&
+    isset($_POST['password']) && $_POST['password'] !== ""
+) {
+    if(isset($_POST['submit'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $result=$connection->prepare("SELECT * FROM users WHERE email=? AND password=?");
+        $result->bindValue(1,$email);
+        $result->bindValue(2,$password);
+        $result->execute();
+        if ($result->rowCount()>=1) {
+            $_SESSION['user']=$_POST['email'];
+            // print_r($_SESSION);
+            header("location:panel\index.php");
+        }else{
+            $error="email or password is incorrect";
+        }
+    }
+}else{
+    if(!empty($_POST)){
+        $error="fill the form";
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,23 +56,29 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>ورود به حساب کاربری</title>
 </head>
+
 <body>
     <section class="d-flex justify-content-center align-items-center min-h-screen bg">
         <div id="overlay"></div>
         <div class="form-container">
-            <form action="#">
+            <form action="#" method="post">
+                <div style="color: red;">
+                    <?php 
+                        if($error!=="")echo $error;
+                    ?>
+                </div>
                 <h1 class="title">ورود به حساب کاربری</h1>
                 <div class="mt-3 position-relative">
-                    <input type="email" class="field" placeholder="ایمیل ...">
+                    <input type="email" name="email" class="field" placeholder="ایمیل ...">
                     <i class="fa fa-user field_icon"></i>
                 </div>
                 <div class="mt-3 position-relative">
-                    <input type="password" class="field" id="fieldPass" placeholder="رمز عبور ...">
+                    <input type="password" name="password" class="field" id="fieldPass" placeholder="رمز عبور ...">
                     <i class="fa fa-lock field_icon"></i>
                     <button type="button" id="showPass"></button>
                 </div>
                 <div class="mt-3">
-                    <button type="submit" class="btn-submit bg-primary">
+                    <button type="submit" name="submit" class="btn-submit bg-primary">
                         <i class="fa fa-sign-in ms-1"></i>
                         <span>ورود به حساب کاربری</span>
                     </button>
@@ -48,4 +94,5 @@
     <script src="js/showPassword.js"></script>
     <script src="js/darkMode.js"></script>
 </body>
+
 </html>
