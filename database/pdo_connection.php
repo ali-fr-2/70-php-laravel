2d1ae87b7174f8b844dff1535297d496b690b88d
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-17 13:17:51 +0330

    first commit

---
+++ database/pdo_connection.php
@@ -0,0 +1,18 @@
+<?php 
+
+$hostName="localhost";
+$userName="root";
+$password="";
+$dbName="codeyad";
+
+try {
+    $connection=new PDO("mysql:host=$hostName;dbname=$dbName",$userName,$password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "success";
+} catch ( PDOException $e) {
+    echo "connection failed".$e->getMessage();
+}
+
+
+
+?>
